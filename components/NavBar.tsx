d6bdfc0f2c563f148bc68394f6d400f9f7b611a3
Liam Parry <me.khanhphm@gmail.com>
2025-02-18 09:52:10 +0700

    Project Base

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="grid grid-cols-12 gap-3 text-center font-bold sans">
+      <div>Home</div>
+      <div>Feature</div>
+      <div>About Us</div>
+      <div>Donate</div>
+    </div>
+  );
+};
+
+export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-roboto-mono)", "monospace"],
+        slab: ["var(--font-roboto-slab)", "serif"],
       },
     },
   },
